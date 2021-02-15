42e46923e3b1b85d2682fcf719e68bcd4cf176cc
Wellidontcare <djjorisdj@gmail.com>
2021-02-15 10:46:41 +0100

    fix script crashing when APOD is a video

--- NASA.py
+++ NASA.py
@@ -14,40 +14,41 @@ def main():
     response = requests.get(url)
     json_response = response.json()
 
-    user = getpass.getuser()
-
     date = json_response["date"]
     explanation = json_response["explanation"]
-    img_url = json_response["hdurl"]
+    img_url = json_response["hdurl"] if json_response["media_type"] == "image" else None
     formated_explanation = textwrap.fill(explanation)
-    image = requests.get(img_url)
-    image_path = (
-        Path.home() / "Downloads" / ("IOTD" + str(random.randint(0, 20)) + ".jpg")
-    )
-    with open(image_path, "wb") as image_file:
-        for chunk in image.iter_content(1024):
-            image_file.write(chunk)
+    if img_url:
+        image = requests.get(img_url)
+        image_path = (
+            Path.home() / "Downloads" / ("IOTD" + str(random.randint(0, 20)) + ".jpg")
+        )
+        with open(image_path, "wb") as image_file:
+            for chunk in image.iter_content(1024):
+                image_file.write(chunk)
+
+        subprocess.run(
+            [
+                "dbus-send",
+                "--session",
+                "--dest=org.kde.plasmashell",
+                "--type=method_call",
+                "/PlasmaShell",
+                "org.kde.PlasmaShell.evaluateScript",
+                'string: var Desktops = desktops();for (i=0;i<Desktops.length;i++) {d = Desktops[i];d.wallpaperPlugin = "org.kde.image";d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");d.writeConfig("Image", "file:'
+                + image_path.as_posix()
+                + '");}',
+            ]
+        )
+    else:
+        print("Current image of the day is a video, please try again tomorrow.")
 
     with open(Path.home() / "Downloads" / "Description.txt", "w") as description:
         description.write(formated_explanation)
 
     print("\n" + formated_explanation + "\n")
     print(date + "\n")
-    print("\n" + img_url)
-
-    subprocess.run(
-        [
-            "dbus-send",
-            "--session",
-            "--dest=org.kde.plasmashell",
-            "--type=method_call",
-            "/PlasmaShell",
-            "org.kde.PlasmaShell.evaluateScript",
-            'string: var Desktops = desktops();for (i=0;i<Desktops.length;i++) {d = Desktops[i];d.wallpaperPlugin = "org.kde.image";d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");d.writeConfig("Image", "file:'
-            + image_path.as_posix()
-            + '");}',
-        ]
-    )
+    print("\n" + img_url if img_url else json_response["url"])
 
     print("\n\nDescription and image-file can be found in the Downloads folder")
 
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # nasa_iotd
-Python script to set the background on KDE Plasma to the NASA Image of the Day
+Python script to set the background on KDE Plasma to the NASA Image of the Day.
